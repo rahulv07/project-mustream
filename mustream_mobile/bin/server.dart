01458863a979d16bd2823c5d069ffd648c0ff48f
Rahul <rahulsudhan2601@gmail.com>
2022-01-31 23:51:37 +0530

    Initial server and client setup test

---
+++ mustream_mobile/bin/server.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+void main() async {
+  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 5902);
+
+  print("Server Started");
+
+  server.listen((client) {
+    handleConnection(client);
+  });
+}
+
+void handleConnection(Socket client) {
+  print('Connection from'
+      ' ${client.remoteAddress.address}:${client.remotePort}');
+
+  // listen for events from the client
+  client.listen(
+    (Uint8List data) async {
+      await Future.delayed(Duration(seconds: 1));
+      final message = String.fromCharCodes(data);
+      print(message);
+    },
+
+    // handle errors
+    onError: (error) {
+      print(error);
+      client.close();
+    },
+
+    // handle the client closing the connection
+    onDone: () {
+      print('Client left');
+      client.close();
+    },
+  );
+}
--- mustream_pc/client_file.py
+++ mustream_pc/client_file.py
@@ -1,6 +1,17 @@
-import wave
-import numpy as np
+# import wave
+# import numpy as np
 
-bytesStream = np.fromfile("temp.wav",dtype=np.uint8)
+#bytesStream = np.fromfile("temp.wav",dtype=np.uint8)
 
-print(bytesStream[0:11])
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 5902        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
