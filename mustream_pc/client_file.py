1b88699c8398af85c92e85be8aa0dd86f7f2684f
Rahul <rahulsudhan2601@gmail.com>
2022-02-02 16:21:44 +0530

    Transfer full audio to mobile and play

--- mustream_pc/client_file.py
+++ mustream_pc/client_file.py
@@ -1,25 +1,25 @@
 import socket
-import numpy as np
-import time
+import wave
 
-HOST = '192.168.24.40'  # The server's hostname or IP address. Localhost : 127.0.0.1
-PORT = 5902        # The port used by the server
+wf = wave.open("temp.wav")
 
-wf = open("temp.wav","rb")
+(nChannels,sampWidth,fs,nFrames,_,_) = wf.getparams()
+
+buffer = sampWidth * fs
+
+HOST = '192.168.87.161' 
+PORT = 5902
+
+f = open("temp.wav","rb")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    buffer = 1024
     i = 0
     while True:
-        packet = wf.read(buffer)
+        packet = f.read(buffer)
         if not packet:
             break
         s.sendall(packet)
         i = i+1
         print(f"Sending packet: {i}")
-        time.sleep(1)
-        
-print("Transfer done")
 
-bytesStream = np.fromfile("temp.wav",dtype=np.uint8)
-print(len(bytesStream))
+print("Transfer done")
